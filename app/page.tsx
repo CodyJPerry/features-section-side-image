1f08e74169aa397eb8cb71e48a9e5834e7a111ab
Cody Perry <perrysdevelopment@gmail.com>
2024-06-09 00:35:28 -0400

    Setup other route and add styles to section

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main>
-      <h1>Hello there!</h1>
+    <main class='m-4'>
+      <section className="grid grid-cols-1 grow bg-white rounded shadow-sm">
+        <h1>Hello there</h1>
+      </section>
     </main>
   );
 }
