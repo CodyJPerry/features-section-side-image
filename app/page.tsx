97316b421c508c129125db2d381e72a3230ebaf8
Cody Perry <perrysdevelopment@gmail.com>
2024-06-10 19:42:58 -0400

    Fix the styling for large

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { RiRocket2Line, RiP2pLine, RiCopyrightLine } from 'react-icons/ri';
 export default function Home() {
   return (
     <main class='m-4'>
-      <section className='grid grid-cols-1 grow bg-white rounded shadow-sm px-3 py-12'>
+      <section className='grid grid-cols-1 grow bg-white rounded shadow-sm px-3 py-12 md:py-16 lg:py-24 lg:px-24'>
         <div className='flex flex-col'>
           <h1 className='text-3xl md:text-5xl font-semibold text-center text-neutral-900 order-1 mt-3 mb-5'>
             Convenience and licensing that empowers
@@ -16,9 +16,11 @@ export default function Home() {
             In a world where storytelling constantly evolves, don&apos;t let
             licensing and poor support hold you down.
           </p>
-          <figure className='relative order-3 my-12'>
+        </div>
+        <div className='lg:grid lg:grid-cols-2 lg:gap-8 lg:mt-16'>
+          <figure className='relative order-3 lg:order-1 my-12 lg:my-0'>
             <Image
-              className='rounded-lg shadow-lg object-cover w-full max-h-[180px] sm:max-h-[300px] md:max-h-[394px] lgmd:max-h-[480px]'
+              className='rounded-lg shadow-lg object-cover w-full max-h-[180px] sm:max-h-[300px] md:max-h-[394px] lgmd:max-h-[460px]'
               src='/featured-left.jpg'
               alt='Abstract features image'
               width={319}
@@ -30,9 +32,7 @@ export default function Home() {
               Abstracted features section image
             </figcaption>
           </figure>
-        </div>
-        <div>
-          <ul className='flex flex-col gap-10'>
+          <ul className='flex flex-col gap-10 lg:order-2'>
             <li className='flex gap-x-5'>
               <span className='h-12 w-12 bg-white shadow rounded-full flex items-center justify-center p-3'>
                 <RiRocket2Line className='text-indigo-700 h-6 w-6' />
